db2620a20faad050ea9ddb5d7da7e1d19b0a7696
Dipjyoti Metia <dipjyotimetia@gmail.com>
2022-05-04 23:49:28 +1000

    update

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -11,7 +11,7 @@ export REPO_OWNER=${3}
 echo 'Initializing...'
 existcode=0
 set -o pipefail
-GITHUB_TOKEN=$TOKEN GITHUB_REPOSITORY=$REPO OWNER=$REPO_OWNER ./app/jarvis
+GITHUB_TOKEN=$TOKEN GITHUB_REPOSITORY=$REPO OWNER=$REPO_OWNER ./jarvis
 if [ $? != 0 ]; then
     existcode=1
 fi
