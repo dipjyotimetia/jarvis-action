693bb0bd19ff68184694770259119ce4044c54cd
Dipjyoti Metia <dipjyotimetia@gmail.com>
2022-05-05 10:56:07 +1000

    update

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-timestamp(){
-    date +"%Y-%m-%d %H:%M:%S"
-}
+cd "${GITHUB_WORKSPACE}" || exit 2
 
 export TOKEN=${1}
 export REPO=${2}
@@ -11,7 +9,7 @@ export REPO_OWNER=${3}
 echo 'Initializing...'
 existcode=0
 set -o pipefail
-GITHUB_TOKEN=$TOKEN GITHUB_REPOSITORY=$REPO OWNER=$REPO_OWNER ./jarvis
+GITHUB_TOKEN=$TOKEN GITHUB_REPOSITORY=$REPO OWNER=$REPO_OWNER /jarvis
 if [ $? != 0 ]; then
     existcode=1
 fi
